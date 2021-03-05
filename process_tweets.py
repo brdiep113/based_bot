942e85910b42bad7e46d9cd295e6a7462e39526a
Eric Tang <etang831@gmail.com>
2021-03-05 12:31:08 -0500

    Added cleaning of users

--- process_tweets.py
+++ process_tweets.py
@@ -34,6 +34,37 @@ def process_tweets(fpath):
     
     return pd.DataFrame(tweet_dict_list)
 
+def process_users(fpath):
+    """ Processes twitter users into a DataFrame
+
+    Args:
+        fpath (string): Relative filepath to users JSON
+        
+    Returns:
+        df_user (DataFrame): DataFrame with user id and name information
+    """
+    
+    path = os.path.relpath(fpath)
+    
+    users_file = open(path)
+    
+    line = users_file.readline()
+    
+    user_dict_list = []
+    
+    while line:
+        user_dict = json.loads(line)
+        clean_user_dict = {
+            'id' : user_dict['id'],
+            'name' : user_dict['name']
+        }
+        
+        user_dict_list.append(clean_user_dict)
+        
+        line = users_file.readline()
+    
+    return pd.DataFrame(user_dict_list)
+
 def save_df(df, fname):
     """ Saves the given dataframe at a given relative file path as a JSON
 
@@ -46,5 +77,5 @@ def save_df(df, fname):
     df.to_json(fname)
 
 if __name__ == '__main__':
-    df_tweet = process_tweets('tweet_data/tweets.json')
-    save_df(df_tweet, 'tweet_data/processed/clean_tweets.json')
+    df_tweet = process_users('tweet_data/users.json')
+    save_df(df_tweet, 'tweet_data/processed/clean_users.json')
